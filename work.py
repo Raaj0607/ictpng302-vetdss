0bb4c55af559238121fc8fbc603182d527b0e779
EC2 Default User <ec2-user@ip-172-31-26-229.ap-southeast-2.compute.internal>
2023-09-08 04:54:22 +0000

    8/09

---
+++ work.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+import math
+
+n = int(input("Enter a number: "))
+
+for t in range( 1, n + 1):
+    print("the number is " + str(t) + "it's square is " + str((int(math.pow(t, 2))) + " and its cube is " + str(t ** 3) + ".")   
+    
